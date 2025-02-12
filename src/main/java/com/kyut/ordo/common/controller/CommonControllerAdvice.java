10f7a8af2612e0b85eee6e323b9fb4336e57d6d1
kyut <yura.krishtof@gmail.com>
2025-02-12 18:19:15 +0200

    OpenApiConfiguration

---
+++ src/main/java/com/kyut/ordo/common/controller/CommonControllerAdvice.java
@@ -0,0 +1,18 @@
+package com.kyut.ordo.common.controller;
+
+import com.kyut.ordo.common.dto.ErrorResponse;
+import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Hidden
+@ControllerAdvice
+public class CommonControllerAdvice {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+    }
+}
---
+++ src/main/java/com/kyut/ordo/common/openApi/OpenApiConfig.java
@@ -0,0 +1,30 @@
+package com.kyut.ordo.common.openApi;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(PageableOpenApiCustomizer pageableOpenApiCustomizer) {
+        OpenAPI openAPI = new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .info(new Info().title("Scheduler API").version("0.0.1"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
+
+        pageableOpenApiCustomizer.customise(openAPI);
+
+        return openAPI;
+    }
+
+}
