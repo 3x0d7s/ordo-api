d87dea64c71c1db78b5d3941c90810b4a949065f
kyut <yura.krishtof@gmail.com>
2025-08-02 12:45:11 +0300

    removed @Data annotation for UserEntity

--- src/main/java/com/kyut/ordo/user/entity/UserEntity.java
+++ src/main/java/com/kyut/ordo/user/entity/UserEntity.java
@@ -1,28 +1,29 @@
 package com.kyut.ordo.user.entity;
 
-import java.time.LocalDateTime;
-import java.util.Collection;
-import java.util.List;
-
 import com.kyut.ordo.auth.provider.common.AuthProvider;
 import com.kyut.ordo.board.entity.BoardMemberEntity;
-import com.kyut.ordo.comment.entity.CommentEntity;
 import com.kyut.ordo.card.entity.CardEntity;
+import com.kyut.ordo.comment.entity.CommentEntity;
 import com.kyut.ordo.workspace.entity.WorkspaceEntity;
 import com.kyut.ordo.workspace.entity.WorkspaceMemberEntity;
-
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-@Data
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Entity
 @Builder
 @Table(name = "users", 
@@ -121,4 +122,19 @@ public class UserEntity implements UserDetails {
         return email;
     }
 
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        UserEntity that = (UserEntity) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
